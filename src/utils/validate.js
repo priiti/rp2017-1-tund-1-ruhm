541cf921d35b75723f0beb76a38a33e3a0338f11
Priit Pärl <priitparl@gmail.com>
2017-10-29 20:29:12 +0200

    validation

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,6 @@
 const { body, validationResult } = require('express-validator/check')
 
-const validationCheck = (req, res, next) => {
+const errorCheck = (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() })
@@ -11,13 +11,11 @@ const validationCheck = (req, res, next) => {
 exports.topic = [
   body('name').exists().withMessage('Name must exist.')
   .isLength({ min: 4 }).withMessage('Name must be at least 4 characters long.'),
-  validationCheck
+  errorCheck
 ]
 
 exports.curriculum = [
-  body('curriculum').exists().withMessage('Curriculum name must exist.')
+  body(['curriculum', 'manager']).exists().withMessage('Curriculum name must exist.')
   .isLength({ min: 4 }).withMessage('Curriculum name must be at least 4 characters long.'),
-  body('manager').exists().withMessage('Manager name must exist')
-  .isLength({ min: 4 }).withMessage('Manager name must be at least 4 characters long.'),
-  validationCheck
+  errorCheck
 ]
